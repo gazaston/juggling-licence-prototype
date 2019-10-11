b8fdb037d465592c4fe92336908e4873061eb502
Gaz Aston <gaz@dxw.com>
2019-10-11 15:02:52 +0100

    added routing for cant juggle page

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,15 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+router.post('/juggling-trick', function (req, res) {
+
+  let canJuggle = req.session.data['how-many-balls']
+
+  if (canJuggle === 'None - I cannot juggle') {
+    res.redirect('/cant-juggle')
+  } else {
+    res.redirect('/juggling-trick')
+  }
+})
+
 module.exports = router
